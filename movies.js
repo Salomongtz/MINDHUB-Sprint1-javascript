0513b1e0195801450a9057f3d7bcac9ec914316c
Salomon Gutierrez <salomon19982009@hotmail.com>
2023-11-06 19:39:21 -0500

    Create cards

--- movies.js
+++ movies.js
@@ -1,21 +1,21 @@
 function createCard(movie) {
     return `
-    <article>
-    <img src="${movie.image}" alt="image">
-    <h3>${movie.title}</h3>
-    <h4>${movie.tagline}</h4>
+    <article class="bg-white flex flex-col truncate  rounded-2xl w-64 h-96 p-4 gap-2">
+    <img class="rounded-md" src="${movie.image}" alt="image">
+    <h3 class="font-bold text-xl">${movie.title}</h3>
+    <h4 class="italic">${movie.tagline}</h4>
     <p>${movie.overview}</p>
     </article>
 `
 }
+
 const cardContainer = document.getElementById("cardContainer")
-function addCard(movieList) {
+function addCard(movieList, container) {
     let template = ""
     for (const movie of movieList) {
         template += createCard(movie)
     }
-    return template
+    container.innerHTML += template
 }
 
-console.log(addCard(movies))
-cardContainer.innerHTML += addCard(movies)
+ addCard(movies, cardContainer)
